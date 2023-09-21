f8ce6c4af5860d4d0c2bd07d0b23616ead23d80c
GuztaJF-DS <noobshippuden13@gmail.com>
2023-09-21 10:33:32 -0300

    feat: added main card: Tablet Size

--- src/components/organisms/MainCard/style.ts
+++ src/components/organisms/MainCard/style.ts
@@ -5,12 +5,14 @@ export const Container = Styled.div`
   display: flex;
   height: 50vw;
   background: #131313;
-  min-height: 50vw;
   position: relative;
   justify-content:center;
   align-items:center;
   img {
     max-width:100vw;
+  }
+  @media (max-width:768px){
+    height: 60vw;
   }
 `;
 
@@ -24,13 +26,17 @@ export const ImageWrapper = Styled.div`
 export const ContentContainer = Styled.div`
   z-index: 1;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   flex-wrap: wrap;
   position: absolute;
   width:30vw;
   height: auto;
   top: 30%;
   left: 4%;
+  @media (max-width:768px){
+    width:40vw;
+    top: 40%;
+  }
 `;
 
 export const DescriptionContainer = Styled.h4`
@@ -41,9 +47,9 @@ export const DescriptionContainer = Styled.h4`
   -webkit-text-stroke-color: #05050571;
   text-shadow: 0.3vw 0.3vw 0.3vw rgb(0 0 0 / 85%);
   margin-bottom: 13px;
-  @media screen and (max-width:768px){
+  @media (max-width:768px){
     margin-bottom: 4px;
-    font-size:1.4vw;
+    font-size:1.8vw;
   }
 `;
 
@@ -52,6 +58,11 @@ export const LogoContainer = Styled.div`
   max-height: 15vw;
   width:30vw;
   position: relative;
+  @media (max-width:768px){
+    min-height: 13vw;
+    max-height: 20vw;
+    width:35vw;
+  }
 `;
 
 export const ButtonContainer = Styled.div`
